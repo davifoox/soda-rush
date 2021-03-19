9fba06b242171d763dbd28e56f4fe7ee7e267f12
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-03-19 17:33:56 -0300

    rotation and impulse using rigid body 2d

--- Assets/Player/PlayerBehaviour.cs
+++ Assets/Player/PlayerBehaviour.cs
@@ -6,6 +6,13 @@ public class PlayerBehaviour : MonoBehaviour
 {
     public float speed = 1000f;
     Vector3 accelerometerVector = new Vector3();
+    Rigidbody2D rb;
+    public float thrust = 0.1f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
@@ -15,11 +22,16 @@ public class PlayerBehaviour : MonoBehaviour
     private void FixedUpdate()
     {
         RotatePlayer(accelerometerVector);
+
+        rb.AddForce(transform.up * thrust);
+        // Alternatively, specify the force mode, which is ForceMode2D.Force by default
+        //rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
     }
 
     public void RotatePlayer(Vector3 rotationVector)
     {
-        Debug.Log(rotationVector.x);
-        transform.Rotate(new Vector3(0, 0, -rotationVector.x) * speed * Time.deltaTime);
+        //Debug.Log(rotationVector.x);
+        //transform.Rotate(new Vector3(0, 0, -rotationVector.x) * speed * Time.deltaTime);
+        rb.rotation -= rotationVector.x * speed * Time.deltaTime;
     }
 }
