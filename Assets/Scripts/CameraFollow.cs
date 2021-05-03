6babeb4e81cca78b89a343805fde29f12c159424
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-05-03 17:42:44 -0300

    event OnPlayerLeftScreen working

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,11 +7,19 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Transform target;
     int offset = 2;
 
+    public delegate void PlayerLeftScreen();
+    public event PlayerLeftScreen OnPlayerLefScreen;
+
     void FixedUpdate()
     {
         if (target.position.y + offset > this.transform.position.y)
             this.transform.position = new Vector3(0, target.position.y + offset, this.transform.position.z);
-        else
+        else if (target.position.y + 5 < this.transform.position.y)
+        {
             Debug.Log("Player out of Screen!");
+            OnPlayerLefScreen();
+        }
+        //Debug.Log("Player posiiton: " + target.transform.position.y);
+        //Debug.Log("Camera posiiton: " + this.transform.position.y);
     }
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] CameraFollow cameraFollow;
+
+    private void OnEnable()
+    {
+        cameraFollow.OnPlayerLefScreen += ReloadCurrentScene;
+    }
+
+    private void OnDisable()
+    {
+        cameraFollow.OnPlayerLefScreen -= ReloadCurrentScene;
+    }
+
+    void ReloadCurrentScene()
+    {
+        Debug.Log("Reloading current scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
