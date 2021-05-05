e08d1752a7906e6b4229c237b5c26007db551ea8
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-05-05 15:19:03 -0300

    menu

---
+++ Assets/Scripts/InitialMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitialMenuManager : MonoBehaviour
+{
+    [SerializeField] Text highscoreText;
+    [SerializeField] Button startButton;
+    [SerializeField] Button configButton;
+    [SerializeField] Button storeButton;
+    [SerializeField] Button creditsButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(() => { StartGame(); });
+    }
+
+    void StartGame()
+    {
+        Debug.Log("START");
+    }
+}
