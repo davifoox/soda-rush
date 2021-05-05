b8e67b31a81fdc9544fcd8c7370808d0625fefaf
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-05-05 16:41:34 -0300

    add - in game UI

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] CameraFollow cameraFollow;
+    [SerializeField] PlayerBehaviour player;
+    [SerializeField] Text scoreText;
+    [SerializeField] Button pauseButton;
+
+    float currentPlayerScore;
+
+    private void Start()
+    {
+        pauseButton.onClick.AddListener(() => { PauseGame(); });
+
+    }
 
     private void OnEnable()
     {
@@ -21,5 +33,25 @@ public class LevelManager : MonoBehaviour
     {
         Debug.Log("Reloading current scene");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void Update()
+    {
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        if(player.transform.position.y > currentPlayerScore)
+        {
+            currentPlayerScore = player.transform.position.y;
+            currentPlayerScore = Mathf.CeilToInt(currentPlayerScore);
+        }
+        scoreText.text = "Score: " + currentPlayerScore;
+    }
+
+    void PauseGame()
+    {
+        Debug.Log("PAUSE");
     }
 }
--- Assets/Scripts/MentosBehaviour.cs
+++ Assets/Scripts/MentosBehaviour.cs
@@ -6,7 +6,6 @@ public class MentosBehaviour : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("AAAA");
         if (collision.gameObject.layer == 8) // Player Layer
         {
             Destroy(this.gameObject);
