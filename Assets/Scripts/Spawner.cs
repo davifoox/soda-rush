96a252c511df885321af16085ba4d9260041ae6a
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-05-18 15:20:30 -0300

    started creating spawner class

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemy; // alterar para Enemy quando criar o script dele
+    [SerializeField] MentosBehaviour mentos;
+    [SerializeField] PlayerBehaviour player;
+    private float offset = 15f;
+
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector2(this.transform.position.x, player.transform.position.y + offset);
+    }
+}
