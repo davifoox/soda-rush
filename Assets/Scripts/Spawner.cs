02412e93926bb979fb3ec3091f9ebef528ea994f
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-05-18 18:14:42 -0300

    spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,8 +9,32 @@ public class Spawner : MonoBehaviour
     [SerializeField] PlayerBehaviour player;
     private float offset = 15f;
 
+    private bool startedSpawning = false;
+
     private void FixedUpdate()
     {
         this.transform.position = new Vector2(this.transform.position.x, player.transform.position.y + offset);
+        if(transform.position.y >= 5 && startedSpawning == false)
+        {
+            StartCoroutine("SpawnerTimer");
+            startedSpawning = true;
+        }
+    }
+
+    IEnumerator SpawnerTimer()
+    {
+        SpawnPowerUp(0f);
+        yield return new WaitForSeconds(2f);
+        StartCoroutine("SpawnerTimer");
+    }
+
+    void SpawnPowerUp(float xPos)
+    {
+        Instantiate(mentos, new Vector3(xPos, transform.position.y, 0), Quaternion.identity);
+    }
+
+    void SpawnObstacle(float xPos)
+    {
+        Instantiate(enemy, new Vector3(xPos, transform.position.y, 0), Quaternion.identity);
     }
 }
