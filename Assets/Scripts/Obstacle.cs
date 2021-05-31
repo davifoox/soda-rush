e1034515483c7d21f43ba5a10700c6e5a3bbe9ac
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-05-31 13:25:47 -0300

    add - airplane sound to game

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     public Transform player;
+    private float offset = 5f;
 
     private void FixedUpdate()
     {
-        if (player.position.y > this.transform.position.y + 5f)
+        if (player.position.y > this.transform.position.y + offset)
             Destroy(this.gameObject);
     }
 }
