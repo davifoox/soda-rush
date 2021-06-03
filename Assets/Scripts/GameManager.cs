383b4a64cdb03fe4e44df9a81a1cd0e1e1509a00
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-06-03 14:58:27 -0300

    disable screen dimming

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,6 +24,8 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        // Disable screen dimming
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
         Vibration.Init();
     }
 
