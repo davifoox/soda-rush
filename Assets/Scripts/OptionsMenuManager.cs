78ee9e238f63eec2253789170bf1c8e2dec6045a
Davi Fox <37807410+davifoox@users.noreply.github.com>
2021-06-08 12:33:34 -0300

    started creating options menu

---
+++ Assets/Scripts/OptionsMenuManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenuManager : MonoBehaviour
+{
+    [SerializeField] Slider musicVolume;
+    [SerializeField] Slider sfxVolume;
+    [SerializeField] Toggle vibrationToggle;
+    [SerializeField] Toggle screenShakeToggle;
+}
